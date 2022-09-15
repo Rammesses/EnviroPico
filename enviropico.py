3fa6062cf59559ba6dda846a82a32e2ce0bb7391
Joel Hammond-Turner <joel@hammond-turner.org.uk>
2022-09-15 07:43:28 +0100

    Add diagnostic flashes (morse) and tidied up log output.

--- enviropico.py
+++ enviropico.py
@@ -10,6 +10,14 @@ WIFI_MODE_UNKNOWN = 0
 WIFI_MODE_CLIENT = 1
 WIFI_MODE_ACCESSPOINT = 2
 
+FLASH_AP = '.- .--.'
+FLASH_IF = '.. ..-.'
+FLASH_MQTT = '-- --.- - -'
+FLASH_WS = '.-- ...'
+FLASH_OK = '--- -.-'
+FLASH_BAD = '-... .- -..'
+FLASH_CFG = '\'-.-. ..-. --.'
+
 class EnviroPico:
 
     DEFAULT_AP_SSID = f'EnviroPico_{configuration.MACHINE_ID}'
@@ -32,26 +40,34 @@ class EnviroPico:
 
     async def main(self)-> None:
 
+        flasher = asyncio.create_task(self.flasher.run())
+
         config = Configuration()
+        self.flasher.set_pattern(FLASH_CFG)
         config.load(configuration.DEFAULT_CONFIG_FILE)
 
         wlan : network.WLAN = None  # type: ignore
         
         if (config.wifi.isValid()):
+            self.flasher.set_pattern(FLASH_IF)
             wlan = await self.connect_to_wifi(config.wifi.ssid, config.wifi.password)
         
         if (wlan == None or  # type: ignore
             wlan.status() != network.STAT_GOT_IP):  # type: ignore
             print('No Wifi configuration - running in AP mode')
+            self.flasher.set_pattern(FLASH_AP)
             wlan = await self.connect_as_ap()
 
         print(wlan.ifconfig())
+
+        self.flasher.set_pattern(FLASH_WS)
 
         (ip, subnet, gateway, dns) = wlan.ifconfig()
         configServer = ConfigurationServer(ip, ConfigurationServer.DEFAULT_PORT, self.isSetupMode())
 
         webserver = asyncio.create_task(configServer.run())
-        flasher = asyncio.create_task(self.flasher.run())
+
+        self.flasher.set_pulse(1000)
 
         await asyncio.gather(
             webserver,
@@ -59,6 +75,7 @@ class EnviroPico:
         
     async def connect_as_ap(self) -> network.WLAN:
         # Set up AP mode
+
         ap = network.WLAN(network.AP_IF)
         ap.config(ssid=self.DEFAULT_AP_SSID, key=self.DEFAULT_AP_PWD) 
         ap.active(True)
@@ -69,35 +86,33 @@ class EnviroPico:
         print("Access point active")
 
         self.wifiMode = WIFI_MODE_ACCESSPOINT
-        self.flasher.set_pattern(flasher.FLASH_AP)
 
         return ap
 
     async def connect_to_wifi(self, ssid : str, password : str) -> network.WLAN:
 
-        print(f'Connecting to {ssid} ({password})...')
+        print(f'Connecting to {ssid}...')
         wlan = network.WLAN(network.STA_IF)
         wlan.active(True)
         wlan.connect(ssid, password)
 
         # Wait for connect or fail
         max_wait : int = 10
-        print('waiting for connection...')
+        print('Waiting for connection.', end='')
         while (max_wait):
             if (wlan.status() in {network.STAT_IDLE, network.STAT_CONNECTING }):   # type: ignore
                 break
             max_wait -= 1  # type: ignore
-            print(f'waiting for connection... {wlan.status()}')
+            print(f'.', end='')
             await asyncio.sleep(1)
 
         # Handle connection error
         if (wlan.status() != network.STAT_GOT_IP):  # type: ignore
             raise RuntimeError('WiFi connection failed')
         else:
-            print(f'Connected to {ssid}.')
+            print(f' Connected.')
 
         self.wifiMode = WIFI_MODE_CLIENT
-        self.flasher.set_pattern(flasher.FLASH_IF)
         return wlan
 
     def isSetupMode(self):
--- flasher.py
+++ flasher.py
@@ -2,17 +2,15 @@
 import uasyncio as asyncio
 from machine import Pin
 
-FLASH_AP = '.- .--.'
-FLASH_IF = '.. ..-.'
-FLASH_MQTT = '-- --.- - -'
-FLASH_RUN = '.-. ..- -.'
-FLASH_OK = '--- -.-'
-FLASH_BAD = '-... .- -..'
-
 FLASHMODE_NONE = 0
 FLASHMODE_PULSE = 1
 FLASHMODE_PATTERN = 2
+FLASHMODE_SOLID = 3
 
+MORSE_DOT_PERIOD = 200
+MORSE_DASH_PERIOD = 400
+MORSE_SPACE_PERIOD = 400
+MORSE_GAP_PERIOD = 200
 class Flasher:
 
     def __init__(self) -> None:
@@ -26,13 +24,14 @@ class Flasher:
             if (self.mode == FLASHMODE_NONE):  # type: ignore
                 self.led.off()
 
+            if (self.mode == FLASHMODE_SOLID):
+                self.led.on()
+
             if (self.mode == FLASHMODE_PULSE):  # type: ignore
                 self.led.toggle()
 
             if (self.mode == FLASHMODE_PATTERN):  # type: ignore
-                # TODO Flash the pattern
-                self.led.on()
-                pass
+                await self.show_pattern(self.pattern)
 
             await asyncio.sleep_ms(self.period)
 
@@ -43,3 +42,21 @@ class Flasher:
     def set_pulse(self, period : int) -> None:
         self.period = period
         self.mode = FLASHMODE_PULSE
+
+    async def show_pattern(self, pattern : str) -> None:
+        for c in pattern:
+            if (c == '.'):
+                self.led.on()
+                await asyncio.sleep_ms(MORSE_DOT_PERIOD)
+                self.led.off()
+
+            if (c == '-'):
+                self.led.on()
+                await asyncio.sleep_ms(MORSE_DASH_PERIOD)
+                self.led.off()
+
+            if (c == ' '):
+                self.led.off()
+                await asyncio.sleep_ms(MORSE_SPACE_PERIOD)
+            
+            await asyncio.sleep_ms(MORSE_GAP_PERIOD)
